37516fd4a734334c4096cc2d5261683c7108e57e
amarenugu <ammi.mailme@gmail.com>
2026-05-25 14:55:53 -0700

    Data types arrays

---
+++ 03-data-types.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+NUM1=10
+NUM2=20
+
+SUM=$((NUM1 + NUM2))
+
+echo "SUM is $SUM"
+
+#ARRAY
+
+MOVIEIS=("RRR" "KGF" "PUSHPA" "BAAHUBALI"
+echo "MOVIEIS is ${MOVIEIS[@]}"
+echo "First movie is ${MOVIEIS[0]}"
+echo "Second movie is ${MOVIEIS[1]}"
+echo "Third movie is ${MOVIEIS[2]}"
+echo "Fourth movie is ${MOVIEIS[3]}")
