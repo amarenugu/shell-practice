ca468041cc12f9f4df915dc0df4426e6b9c7a164
amarenugu <ammi.mailme@gmail.com>
2026-05-24 22:16:16 -0700

    second script

--- secon_script.sh
+++ secon_script.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 echo "This is the second script."
 echo "It is being called from the first script."   
+echo "The current working directory is: $(pwd)"
+echo "The current date and time is: $(date)"
+echo "The user running this script is: $(whoami)"
+
 
