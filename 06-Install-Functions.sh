2da256c416e3922f9ef7dc61723391997304a30c
amarenugu <ammi.mailme@gmail.com>
2026-05-25 18:16:03 -0700

    Modified Install Function Script

--- 06-Install-Functions.sh
+++ 06-Install-Functions.sh
@@ -7,3 +7,27 @@ if [ $USERID -ne 0 ]; then
     echo "Please run this script as root."
     exit 1
 fi
+
+# first arg --> What are you trying to install
+# second arg --> exit code
+
+VALIDATE () {
+    if [ $2 -ne 0 ]; then
+        echo "Failed to install $1. Please check the logs for more details."
+        exit 1
+    else
+        echo "$1 installed successfully."
+    fi
+)
+
+echo " I am continuing with the installation of $1"
+
+dnf list installed | grep $1
+VALIDATE "$1" $?
+
+if [ $2 -ne 0 ]; then
+    echo "Failed to install $1. Please check the logs for more details."
+    exit 1
+else
+    echo "$1 installed successfully."
+fi
