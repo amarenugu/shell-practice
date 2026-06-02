4419e4edacf78d87f66c6abc536666d480025585
amarenugu <ammi.mailme@gmail.com>
2026-06-02 12:51:12 -0700

    New file loops.sh

--- 08-loops.sh
+++ 08-loops.sh
@@ -11,3 +11,17 @@ if [ $USERID -ne 0 ]; then
     echo " Please run this script with the root access"
     exit 1
 fi
+
+for package in $@
+do
+    echo "$TIMESTAMP [INFO] Installing $package
+    dng list installed $package &>> $LOGS_FILE
+    if [ $? -ne 0 ]; then
+        dnf install $package -y &>> $LOGS_FILE
+        if [ $? -eq 0 ]; then
+            echo "Installation of $package completed successfully"
+        else 
+            echo "Installation of $package Failed or already installed"
+        fi
+    fi
+done
