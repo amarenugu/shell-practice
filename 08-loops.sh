efad8aa8ba13b59b1307bec13f309ffa068fa7f7
amarenugu <ammi.mailme@gmail.com>
2026-06-02 12:56:20 -0700

    New file loops.sh

--- 08-loops.sh
+++ 08-loops.sh
@@ -14,7 +14,7 @@ fi
 
 for package in $@
 do
-    echo "$TIMESTAMP [INFO] Installing $package
+    echo "$TIMESTAMP [INFO] Installing $package "
     dnf list installed $package &>> $LOGS_FILE
     if [ $? -ne 0 ]; then
         dnf install $package -y &>> $LOGS_FILE
