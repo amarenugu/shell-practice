74f0ff88f5e53a8dba07658f623008cc7cba7fd1
amarenugu <ammi.mailme@gmail.com>
2026-06-03 13:00:16 -0700

    Modified Install Function Script

--- Colors.sh
+++ Colors.sh
@@ -11,6 +11,13 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+#Check the root access
+
+if [ $USERID -ne 0 ] then
+    echo -e "Please run the script with $R ROOT $N"
+    exit 1
+fi
+
 
 echo -e "$G $TIME_STAMP $N"
 echo -e "$Y $LOGS_FILE $N"
