0e6ae992d858640e38d26026592c94e7873028b7
amarenugu <ammi.mailme@gmail.com>
2026-06-03 10:24:23 -0700

    Modified Install Function Script

--- Colors.sh
+++ Colors.sh
@@ -6,6 +6,12 @@ USERID=$(id -u)
 LOGS_DIR="/var/log/shell-script"
 LOGS_FILE="$LOGS_DIR/$0.log"
 TIME_STAMP=$(date "+%Y-%m-%d-%H-%M-%S")
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
-echo "$TIME_STAMP"
-echo "$LOGS_FILE"
+
+echo "$G $TIME_STAMP $N"
+echo "$Y $LOGS_FILE $N"
+
